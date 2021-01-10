72fb1b4cdef38d2a01893f8df239efe3e6c4c339
Marussja <taty-yona@yandex.ru>
2021-01-10 16:51:25 +0700

    Доверстать главную страницу_правка

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,14 +1,14 @@
-var navMain = document.querySelector('.main-navigation');
-var navToggle = document.querySelector('.main-navigation__toggle');
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('main-navigation--nojs');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if(navMain.classList.contains('main-navigation--closed')) {
-    navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+  if(navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
